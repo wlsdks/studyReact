da020d20459d662317fed0d440724b2389a3feab
jinan <ja.choi@sweetk.co.kr>
2022-08-06 00:23:59 +0900

    1.검색시마다 최근 검색어 목록에 추가된다.

--- js/Store.js
+++ js/Store.js
@@ -1,3 +1,4 @@
+import { createNextId } from "./helpers.js";
 import { TabType } from "./views/TabView.js";
 
 const tag = "[store]";
@@ -18,6 +19,7 @@ export default class Store {
     this.searchResult = this.storage.productData.filter((product) => 
       product.name.includes(keyword)
       );
+    this.addHistory(keyword);
   }
 
   getKeywordList(){
@@ -35,6 +37,23 @@ export default class Store {
   removeHistory(keyword) {
     this.storage.historyData = this.storage.historyData.filter(
       (history) => history.keyword !== keyword);
+  }
+
+  addHistory(keyword){
+    keyword = keyword.trim();
+    if(!keyword){
+      return;
+    }
+
+    const hasHistory = this.storage.historyData.some(history => history.keyword === keyword);
+    if(hasHistory){
+      this.removeHistory(keyword);
+    }
+
+    const id = createNextId(this.storage.historyData)
+    const date = new Date();
+    this.storage.historyData.push({id, keyword, date});
+    this.storage.historyData = this.storage.historyData.sort(this._sortHistory);
   }
 
 }
